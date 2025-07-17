44d288fb7c993f69bf9262abdcb69499e0544fee
Tomas178 <tomaspetronis77@gmail.com>
2025-07-17 21:37:46 +0300

    (Feat): add a class for handling errors when Resource is not found

---
+++ src/utils/errors/NotFound.ts
@@ -0,0 +1,11 @@
+import { Status } from 'discord.js';
+import { StatusCodes } from 'http-status-codes';
+
+export default class NotFound extends Error {
+  status: number;
+
+  constructor(message: string) {
+    super(message);
+    this.status = StatusCodes.NOT_FOUND;
+  }
+}
