ad593371f22f8033bf0df436989eb750a5c8639a
Tomas178 <tomaspetronis77@gmail.com>
2025-07-17 21:44:26 +0300

    (Feat): make constructor argument optional and with a default value

--- src/utils/errors/MethodNotAllowed.ts
+++ src/utils/errors/MethodNotAllowed.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 export default class MethodNotAllowed extends Error {
   status: number;
 
-  constructor(message: string) {
+  constructor(message: string = 'Method not allowed') {
     super(message);
     this.status = StatusCodes.METHOD_NOT_ALLOWED;
   }
