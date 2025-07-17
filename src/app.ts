48dea4b52870876a4e1c7316bb4f479688043fbd
Tomas178 <tomaspetronis77@gmail.com>
2025-07-17 21:56:46 +0300

    (Feat): add the use of jsonErrorHandler middleware for app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
+import 'dotenv/config';
 import express from 'express';
-import { Client, GatewayIntentBits } from 'discord.js';
 import { Database } from './database';
+import jsonErrorHandler from './middleware/jsonErrors';
 
 export default function createApp(db: Database) {
+  const { DISCORD_BOT_TOKEN } = process.env;
+  if (!DISCORD_BOT_TOKEN) {
+    throw new Error('Provide DISCORD_BOT_TOKEN in .env');
+  }
+
   const app = express();
 
-  const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
-
   app.use(express.json());
+
+  app.use(jsonErrorHandler);
 
   return app;
 }
