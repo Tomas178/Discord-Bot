1ecf3ec46bd6a5ee85ba178e042e5e4835f54571
Tomas178 <tomaspetronis77@gmail.com>
2025-07-18 12:59:21 +0300

    (Update): added additional tests for parseId and seperated tests for parse

--- src/modules/sprints/tests/schema.spec.ts
+++ src/modules/sprints/tests/schema.spec.ts
@@ -1,25 +1,70 @@
 import { omit } from 'lodash/fp';
-import { parse, parseInsertable, parseUpdateable } from '../schema';
+import { parse, parseId, parseInsertable, parseUpdateable } from '../schema';
 import { fakeSprintFull } from './utils';
 
-it('parses a valid record', () => {
-  const record = fakeSprintFull();
+describe('parse', () => {
+  it('parses a valid record', () => {
+    const record = fakeSprintFull();
 
-  expect(parse(record)).toEqual(record);
+    expect(parse(record)).toEqual(record);
+  });
+
+  it('throws an error due to empty sprintCode', () => {
+    const sprintWithEmptySprintCode = fakeSprintFull({
+      sprintCode: '',
+    });
+
+    expect(() => parse(sprintWithEmptySprintCode)).toThrow(/sprintCode/i);
+  });
+
+  it('throws an error due to missing sprintCode', () => {
+    const sprintWithoutSprintCode = omit(['sprintCode'], fakeSprintFull());
+
+    expect(() => parse(sprintWithoutSprintCode)).toThrow(/sprintCode/i);
+  });
+
+  it('throws an error due to invalid sprintCode', () => {
+    const sprintWithInvalidSprintCode = fakeSprintFull({
+      sprintCode: 'WD1.1',
+    });
+    expect(() => parse(sprintWithInvalidSprintCode)).toThrow(
+      /Sprint code must match the pattern/i
+    );
+  });
 });
 
-it('throws an error due to empty/missing/wrong sprintCode', () => {
+describe('parseId', () => {
+  it('parses a valid id', () => {
+    const id = 1;
+
+    expect(parseId(id)).toEqual(id);
+  });
+
+  it('throws an error for id which is float type', () => {
+    const realValueId = 1.1;
+
+    expect(() => parseId(realValueId)).toThrow(/expected int/i);
+  });
+
+  it('throws an error for id which is negative', () => {
+    const negativeId = -1;
+
+    expect(() => parseId(negativeId)).toThrow(/Too small/i);
+  });
+});
+
+it('throws an error due to empty/missing/invalid sprintCode', () => {
   const sprintWithoutSprintCode = omit(['sprintCode'], fakeSprintFull());
   const sprintWithEmptySprintCode = fakeSprintFull({
     sprintCode: '',
   });
-  const sprintWithWrongSprintCode = fakeSprintFull({
+  const sprintWithInvalidSprintCode = fakeSprintFull({
     sprintCode: 'WD1.1',
   });
 
   expect(() => parse(sprintWithoutSprintCode)).toThrow(/sprintCode/i);
   expect(() => parse(sprintWithEmptySprintCode)).toThrow(/sprintCode/i);
-  expect(() => parse(sprintWithWrongSprintCode)).toThrow(
+  expect(() => parse(sprintWithInvalidSprintCode)).toThrow(
     /Sprint code must match the pattern/i
   );
 });
