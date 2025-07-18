22392c69882bfe5c62f659c3ea1ef4b1b3959809
Tomas178 <tomaspetronis77@gmail.com>
2025-07-18 16:46:49 +0300

    (Feat): add SprintAlreadyExists error class

--- src/modules/sprints/errors.ts
+++ src/modules/sprints/errors.ts
@@ -1,5 +1,11 @@
 import NotFound from '@/utils/errors/NotFound';
 
+export class SprintAlreadyExists extends Error {
+  constructor(sprintCode: string) {
+    super(`Sprint with code "${sprintCode}" already exists.`);
+  }
+}
+
 export class SprintNotFound extends NotFound {
   constructor(code: string) {
     super(`Sprint with code ${code} already exists`);
