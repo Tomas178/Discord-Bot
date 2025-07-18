dcd1679167b5d7b5660093deff3d435cd9580680
Tomas178 <tomaspetronis77@gmail.com>
2025-07-18 16:02:37 +0300

    (Update): export RowInsert and  RowUpdate

--- src/modules/sprints/model.ts
+++ src/modules/sprints/model.ts
@@ -5,8 +5,8 @@ import { keys } from './schema';
 const TABLE = 'sprints';
 type Row = Sprints;
 type RowWithoutId = Omit<Row, 'id'>;
-type RowInsert = Insertable<RowWithoutId>;
-type RowUpdate = Updateable<RowWithoutId>;
+export type RowInsert = Insertable<RowWithoutId>;
+export type RowUpdate = Updateable<RowWithoutId>;
 type RowSelect = Selectable<Row>;
 
 export default (db: Database) => ({
@@ -26,29 +26,29 @@ export default (db: Database) => ({
     db.insertInto(TABLE).values(record).returning(keys).executeTakeFirst(),
 
   update: async (
-    id: number,
+    sprintCode: string,
     partial: RowUpdate
   ): Promise<RowSelect | undefined> => {
     if (Object.keys(partial).length === 0) {
       return db
         .selectFrom(TABLE)
         .select(keys)
-        .where('id', '=', id)
+        .where('sprintCode', '=', sprintCode)
         .executeTakeFirst();
     }
 
     return db
       .updateTable(TABLE)
       .set(partial)
-      .where('id', '=', id)
+      .where('sprintCode', '=', sprintCode)
       .returning(keys)
       .executeTakeFirst();
   },
 
-  remove: async (id: number): Promise<RowSelect | undefined> =>
+  remove: async (sprintCode: string): Promise<RowSelect | undefined> =>
     db
       .deleteFrom(TABLE)
-      .where('id', '=', id)
+      .where('sprintCode', '=', sprintCode)
       .returning(keys)
       .executeTakeFirst(),
 });
