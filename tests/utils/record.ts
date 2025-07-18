f633fc04abd1fc3353d124188304972bda687b27
Tomas178 <tomaspetronis77@gmail.com>
2025-07-18 09:46:41 +0300

    (Feat): add a helper function for easier testing

---
+++ tests/utils/record.ts
@@ -0,0 +1,13 @@
+import type { Insertable, Kysely } from 'kysely';
+import { DB } from '@/database';
+
+type HelperType<N extends keyof DB> = { [P in N]: DB[P] };
+
+export const createFor =
+  <N extends keyof DB, T extends HelperType<N>>(db: Kysely<T>, tableName: N) =>
+  (records: Insertable<DB[N]> | Insertable<DB[N]>[]) =>
+    db
+      .insertInto(tableName)
+      .values(records as any)
+      .returningAll()
+      .execute();
