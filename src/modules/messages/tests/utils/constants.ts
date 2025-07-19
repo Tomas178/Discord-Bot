b3ff4c9582746cd81001c1677309c8f23a220524
Tomas178 <tomaspetronis77@gmail.com>
2025-07-19 19:03:37 +0300

    (Feat): add utility functions and constants for easier testing

---
+++ src/modules/messages/tests/utils/constants.ts
@@ -0,0 +1,7 @@
+export const ERROR_EMPTY_MESSAGE = 'Message cannot be empty!';
+export const ERROR_TOO_LONG_MESSAGE = 'Message too long!';
+export const MAX_LENGTH_MESSAGE = 255;
+
+export const MAX_LENGTH_USERNAME = 64;
+export const ERROR_EMPTY_USERNAME = 'Username cannot be empty!';
+export const ERROR_TOO_LONG_USERNAME = 'Username too long!';
---
+++ src/modules/messages/tests/utils/utils.ts
@@ -0,0 +1,28 @@
+import { Messages } from '@/database';
+import { Insertable } from 'kysely';
+
+export const fakeMessage = (
+  overrides: Partial<Insertable<Messages>> = {}
+): Insertable<Messages> => ({
+  message: 'Congrats Tomas!',
+  sprintCode: 'WD-1.1',
+  username: 'username',
+  ...overrides,
+});
+
+export const messageMatcher = (
+  overrides: Partial<Insertable<Messages>> = {}
+) => ({
+  id: expect.any(Number),
+  createdAt: expect.any(String),
+  ...overrides,
+  ...fakeMessage(),
+});
+
+export const fakeMessageFull = (
+  overrides: Partial<Insertable<Messages>> = {}
+) => ({
+  id: 2,
+  createdAt: new Date(2025, 6, 19).toISOString(),
+  ...fakeMessage(overrides),
+});
