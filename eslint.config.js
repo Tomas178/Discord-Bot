cbfec433f510a9562ff21528e04ea0114cbf9c2b
Tomas178 <tomaspetronis77@gmail.com>
2025-07-21 20:41:30 +0300

    (Fix): fix eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: tsparser,
+      sourceType: 'module',
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
+];
