6ab304e5fd131c895cee6b92dfb60bfe65d88502
Tomas178 <tomaspetronis77@gmail.com>
2025-07-22 08:31:10 +0300

    (Fix): change EMBED_COLOR to readable color

--- src/utils/discord/constants.ts
+++ src/utils/discord/constants.ts
@@ -3,4 +3,4 @@ export const ERROR_FAILED_TO_SEND_TO_DISCORD_CHANNEL =
   'Failed to send message to Discord channel';
 export const ERROR_NO_BOT_TOKEN = 'Provide DISCORD_BOT_TOKEN in .env';
 
-export const EMBED_COLOR = 0x00d26a;
+export const EMBED_COLOR = 'DarkGreen';
